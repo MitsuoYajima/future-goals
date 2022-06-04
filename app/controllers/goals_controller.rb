c832fd914355734341419b66752e3586f14adfbd
MitsuoYajima <mit.yaji0904@gmail.com>
2022-06-05 00:11:33 +0900

    仮ビューファイルの表示まで

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,4 +3,7 @@ class GoalsController < ApplicationController
 def index
 end
 
+def new
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get 'post', to: 'goals#new'
+  root to: 'goals#index'
 end
